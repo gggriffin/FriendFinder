e067c7002b93a37accd6a89d0ce448600f2c1f4b
Garrett Griffin <griffin.garrett@gmail.com>
2019-05-14 16:22:23 -0500

    added listen function to server.js at PORT 3000

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+});
