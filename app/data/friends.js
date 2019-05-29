3287b6e17e289c9e62449ec58c67d4fb287ac145
Garrett Griffin <griffin.garrett@gmail.com>
2019-05-29 16:32:38 -0500

    finished initial friends.js friends array

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+const friends = [
+    {
+        name: 'Thomas Turtle',
+        photo: 'https://images.pexels.com/photos/60087/pexels-photo-60087.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        scores: [
+            '2',
+            '5',
+            '1',
+            '4',
+            '5',
+            '1',
+            '2',
+            '1',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Steve Squirrel',
+        photo: 'https://images.pexels.com/photos/416163/pexels-photo-416163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        scores: [
+            '5',
+            '4',
+            '3',
+            '2',
+            '1',
+            '5',
+            '5',
+            '5',
+            '1',
+            '4'
+        ]
+    },
+    {
+        name: 'Benny Bear',
+        photo: 'https://images.pexels.com/photos/213988/pexels-photo-213988.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        scores: [
+            '5',
+            '3',
+            '3',
+            '2',
+            '5',
+            '3',
+            '3',
+            '3',
+            '3',
+            '5'
+        ]
+    },
+    {
+        name: 'Jack Danger, Wonderpug',
+        photo: 'https://images.pexels.com/photos/1851471/pexels-photo-1851471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        scores: [
+            '1',
+            '4',
+            '5',
+            '5',
+            '1',
+            '5',
+            '2',
+            '1',
+            '4',
+            '5'
+        ]
+    }
+];
+
+module.exports = friends;
